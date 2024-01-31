52fabe40c7a7219faba7e7d71763edf6c59dee10
Kyle McDonald <kyle@knock.app>
2024-01-31 15:50:02 -0600

    feat: eslint shared config + lint actions runner

--- packages/postcss-config/src/postcss-config.ts
+++ packages/postcss-config/src/postcss-config.ts
@@ -2,6 +2,7 @@ import { tailwindConfig } from "@telegraph/tailwind-config";
 
 export default {
   plugins: [
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
     require("tailwindcss")({
       config: tailwindConfig,
     }),
--- packages/vitest-config/src/vitest-setup/index.ts
+++ packages/vitest-config/src/vitest-setup/index.ts
@@ -6,5 +6,5 @@ expect.extend(matchers);
  fixes: Error: Not implemented: HTMLCanvasElement.prototype.getContext (without installing the canvas npm package)
  source: https://stackoverflow.com/questions/48828759/unit-test-raises-error-because-of-getcontext-is-not-implemented
 */
-// @ts-expect-error
+// @ts-expect-error -- see above
 HTMLCanvasElement.prototype.getContext = () => {};
