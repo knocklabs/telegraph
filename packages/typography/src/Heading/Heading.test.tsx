4eb4f6978ea48487375d4d7f6cd2cf7134489912
Kyle McDonald <kyle@knock.app>
2024-02-07 11:24:31 -0600

    fix: add required as tag to heading test

--- packages/typography/src/Heading/Heading.test.tsx
+++ packages/typography/src/Heading/Heading.test.tsx
@@ -7,23 +7,23 @@ import { Heading } from "./Heading";
 
 describe("Heading", () => {
   it("should render without a11y issues", async () => {
-    const { container } = render(<Heading>Heading</Heading>);
+    const { container } = render(<Heading as="h1">Heading</Heading>);
     expect(await axe(container)).toHaveNoViolations();
   });
   it("size props applies correct className", () => {
-    const { container } = render(<Heading size="9">Heading</Heading>);
+    const { container } = render(<Heading size="9" as="h1">Heading</Heading>);
     expect(container.firstChild).toHaveClass("text-9");
   });
   it("color props applies correct className", () => {
-    const { container } = render(<Heading color="red">Heading</Heading>);
+    const { container } = render(<Heading color="red" as="h1">Heading</Heading>);
     expect(container.firstChild).toHaveClass("text-red-11");
   });
   it("align props applies correct className", () => {
-    const { container } = render(<Heading align="left">Heading</Heading>);
+    const { container } = render(<Heading align="left" as="h1">Heading</Heading>);
     expect(container.firstChild).toHaveClass("text-left");
   });
   it("default props applies correct className", () => {
-    const { container } = render(<Heading>Heading</Heading>);
+    const { container } = render(<Heading as="h1">Heading</Heading>);
     expect(container.firstChild).toHaveClass("text-gray-12");
     expect(container.firstChild).toHaveClass("text-2");
     expect(container.firstChild).toHaveClass("leading-2");
