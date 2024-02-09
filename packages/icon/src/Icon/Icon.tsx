1cc6f7c693deb84098c69715a406c0926607c58c
Kyle McDonald <kyle@knock.app>
2024-02-09 16:14:09 -0600

    feat: proof of concept of icons working

--- packages/icon/src/Icon/Icon.tsx
+++ packages/icon/src/Icon/Icon.tsx
@@ -1,34 +1,21 @@
-// import { IonIcon } from "ionicons/components/ion-icon.js";
+import { IonIcon } from "@ionic/react";
 import React from "react";
 
-//
-// type IconProps = {
-//   name: string;
-// };
+type IconProps = {
+  icon: string;
+};
 
-// const Icon = React.forwardRef<any, IconProps>(({ name, ...props }, ref) => {
-//   // const [icon, setIcon] = React.useState<any>(null);
-//   //
-//   // React.useEffect(() => {
-//   //   const importIcon = () => {
-//   //     const { default: IconComponent } = require(
-//   //       `ionicons/dist/svg/${name}.svg`,
-//   //     );
-//   //     console.log(IconComponent);
-//   //     setIcon(IconComponent);
-//   //   };
-//   //   importIcon();
-//   // }, [name]);
-//   // console.log(icon);
-//   const icon = document.createElement("ion-icon");
-//   icon.name = "add";
-//
-//   return (
-//     <span>
-//       <span dangerouslySetInnerHTML={{ __html: icon.outerHTML }} />
-//     </span>
-//   );
-// });
+const Icon = React.forwardRef<unknown, IconProps>(({ icon, ...props } ) => {
+  return (
+    <span className="inline-block h-8 w-8">
+      <IonIcon
+        icon={icon}
+        {...props}
+        // will need to do custom icon size tokens here for text, text-icon-1 perhaps
+        className="text-red-5 text-[8rem]"
+      />
+    </span>
+  );
+});
 
-const Icon = () => <div />;
 export { Icon };
