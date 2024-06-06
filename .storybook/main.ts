ef53bf31a55963c95af2a20a13992e4c148ca9bd
Kyle McDonald <kyle@knock.app>
2024-06-06 15:18:30 -0500

    feat: add render modes for light / dark support in storybook (#106)
    ### Description
    - Updates storybook css import to be `default` so we get theme-ing
    - Adds storybook addon to control color scheme to switch between themes
    
    ### Tasks 
    [KNO-6149](https://linear.app/knock/issue/KNO-6149/[telegraph]-light-dark-mode-support-in-storybook)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,11 @@
-
 export default {
   framework: "@storybook/react-vite",
   stories: ["../packages/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-addon-render-modes",
+  ],
   core: {
     builder: "@storybook/builder-vite", // 👈 The builder enabled here.
   },
