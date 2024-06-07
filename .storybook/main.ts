e471f62002d255fe7f36284e1302647d2eaa4380
Kyle McDonald <kyle@knock.app>
2024-06-07 12:38:03 -0500

    fix: pkg postcss config collidng with storybook postcss config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,31 @@
+import type { ViteFinal } from "@storybook/builder-vite";
+import { mergeConfig } from "vite";
+
+const viteFinal: ViteFinal = (config) => {
+  return mergeConfig(config, {
+    css: {
+      postcss: {
+        // Define postcss config inline so we don't need to create a postcss.config.js file
+        // and collide with the package consumer's postcss config
+        plugins: [
+          require("postcss-import-ext-glob"),
+          require("postcss-import"),
+        ],
+      },
+    },
+  });
+};
+
 export default {
   framework: "@storybook/react-vite",
   stories: ["../packages/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-postcss",
-    "storybook-addon-render-modes"
+    "storybook-addon-render-modes",
   ],
   core: {
     builder: "@storybook/builder-vite", // 👈 The builder enabled here.
   },
+  viteFinal,
 };
--- postcss.config.js
+++
@@ -1,4 +0,0 @@
-// Post CSS Config that Storybook uses
-module.exports = {
-  plugins: [require("postcss-import-ext-glob"), require("postcss-import")],
-};
