50db17cabcc413fd3edacaac2d7aa21c61e195d4
Kyle McDonald <kyle@knock.app>
2024-07-10 09:34:00 -0500

    fix: tag tests

--- packages/tag/src/Tag/Tag.test.tsx
+++ packages/tag/src/Tag/Tag.test.tsx
@@ -1,6 +1,5 @@
 import { render } from "@testing-library/react";
-import React from "react";
-import { describe, expect, it } from "vitest";
+import { describe, it } from "vitest";
 import { axe, expectToHaveNoViolations } from "vitest.axe";
 
 import { Tag } from "./Tag";
