a45a1de164a8eb6d904c53b185123f9cd45f1f56
Kyle McDonald <kyle@knock.app>
2024-07-11 15:24:45 -0500

    feat: check package sizes action (#175)
    * feat: check package sizes
    
    * fix: change how script runs
    
    * fix: move script to actions
    
    * fix: change how outputs are working
    
    * fix: syntax error
    
    * fix: syntax error
    
    * fix: remove output line for now
    
    * fix: add actions checkout
    
    * fix: change location of action
    
    * feat: check package sizes
    
    * fix: change how script runs
    
    * fix: move script to actions
    
    * fix: change how outputs are working
    
    * fix: syntax error
    
    * fix: syntax error
    
    * fix: remove output line for now
    
    * fix: add actions checkout
    
    * fix: change location of action
    
    * fix: remove empty input
    
    * fix: packages path
    
    * feat: add compare action
    
    * fix: disable checkout to main
    
    * fix: typo on key
    
    * feat: add pr comment
    
    * fix: update comment formatting
    
    * fix: update formatting
    
    * fix: update formatting
    
    * fix: update formatting
    
    * fix: update formatting
    
    * fix: formatting
    
    * fix: formatting
    
    * fix: formatting
    
    * chore: remove unused package, add back main checkout

---
+++ .github/actions/compare-package-sizes-action/index.mjs
@@ -0,0 +1,62 @@
+import core from "@actions/core";
+
+const SIZE_THRESHOLD = 20;
+
+const main = async () => {
+  const currentPackageSizes = JSON.parse(
+    core.getInput("current-package-sizes")
+  );
+  const previousPackageSizes = JSON.parse(
+    core.getInput("previous-package-sizes")
+  );
+
+  const packageSizeDifferences = currentPackageSizes.map((currentPackage) => {
+    const previousPackage = previousPackageSizes.find(
+      (pkg) => pkg.packageName === currentPackage.packageName
+    );
+    return {
+      packageName: currentPackage.packageName,
+      size: currentPackage.size - previousPackage.size,
+    };
+  });
+
+  const packagesWithThreshold = packageSizeDifferences.filter(
+    (pkg) => Math.abs(pkg.size) > SIZE_THRESHOLD
+  );
+
+  const prComment = `
+    ### Package size differences
+
+${
+  packagesWithThreshold.length > 0
+    ? `The following packages have size differences greater than 20 KB
+
+        ${packagesWithThreshold
+          .map(
+            (pkg) =>
+              `- \`@telegraph/${pkg.packageName}\`: ${pkg.size > 0 ? "+" : ""}${pkg.size} KB`
+          )
+          .join("\n")}
+    `
+    : `No package size differences greater than ${SIZE_THRESHOLD} KB 🚀`
+}
+
+
+<details>
+<summary>All package size differences</summary>
+
+${packageSizeDifferences
+  .map(
+    (pkg) =>
+      `- \`@telegraph/${pkg.packageName}\`: ${pkg.size > 0 ? "+" : ""}${pkg.size} KB`
+  )
+  .join("\n")}
+
+</details>
+
+  `;
+
+  core.setOutput("pr-comment", prComment);
+};
+
+main();
