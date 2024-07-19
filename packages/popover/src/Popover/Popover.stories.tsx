661854eba8553eb7a112d1f3f5f5555a27729581
Connor Lindsey <cblindsey3@gmail.com>
2024-07-19 13:35:03 -0700

    feat: add popover component (#192)
    * feat: add popover component
    
    * add control props and animation
    
    * changeset
    
    * manypkg fix
    
    * handle new package in check sizes action
    
    * pr feedback, move to new style engine
    
    * fix lock file
    
    * update vite config to use style engine

--- packages/popover/src/Popover/Popover.stories.tsx
+++ packages/popover/src/Popover/Popover.stories.tsx
@@ -1,19 +1,15 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "@telegraph/button";
 import type { TgphComponentProps } from "@telegraph/helpers";
+import { Lucide } from "@telegraph/icon";
 import { Stack } from "@telegraph/layout";
 
-import { Tooltip as TelegraphTooltip } from "./Tooltip";
+import { Popover } from "./Popover";
 
 const meta: Meta = {
-  title: "Components/Tooltip",
-  component: TelegraphTooltip,
+  title: "Components/Popover",
+  component: Popover.Root,
   argTypes: {
-    label: {
-      control: {
-        type: "text",
-      },
-    },
     side: {
       options: ["top", "bottom", "left", "right"],
       control: {
@@ -26,31 +22,32 @@ const meta: Meta = {
         type: "select",
       },
     },
-    enabled: {
-      control: {
-        type: "boolean",
-      },
-    },
   },
   args: {
-    label: "Tooltip",
     side: "bottom",
     align: "center",
-    enabled: true,
   },
 };
 
 export default meta;
 
-type Story = StoryObj<TgphComponentProps<typeof TelegraphTooltip>>;
+type Story = StoryObj<TgphComponentProps<typeof Popover.Root>>;
 
 export const Default: Story = {
   render: ({ ...args }) => {
     return (
       <Stack w="full" my="10" align="center" justify="center">
-        <TelegraphTooltip {...args}>
-          <Button color="blue">Hover me</Button>
-        </TelegraphTooltip>
+        <Popover.Root {...args} defaultOpen={true}>
+          <Popover.Trigger asChild={true}>
+            <Button
+              variant="outline"
+              leadingIcon={{ icon: Lucide.Ellipsis, "aria-hidden": true }}
+            />
+          </Popover.Trigger>
+          <Popover.Content {...args} px="4">
+            <p>Test popover content</p>
+          </Popover.Content>
+        </Popover.Root>
       </Stack>
     );
   },
---
+++ packages/popover/src/Popover/Popover.tsx
@@ -0,0 +1,185 @@
+import * as RadixPopover from "@radix-ui/react-popover";
+import { useControllableState } from "@radix-ui/react-use-controllable-state";
+import {
+  RefToTgphRef,
+  TgphComponentProps,
+  TgphElement,
+} from "@telegraph/helpers";
+import { Stack } from "@telegraph/layout";
+import { motion } from "framer-motion";
+import React from "react";
+
+type RootProps = React.ComponentProps<typeof RadixPopover.Root> & {
+  defaultOpen?: boolean;
+};
+
+type PopoverContextProps = {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+};
+
+const PopoverContext = React.createContext<PopoverContextProps>({
+  open: false,
+  setOpen: () => {},
+});
+
+const Root = ({
+  open: openProp,
+  onOpenChange: onOpenChangeProp,
+  defaultOpen: defaultOpenProp,
+  children,
+  ...props
+}: RootProps) => {
+  const [open = false, setOpen] = useControllableState({
+    prop: openProp,
+    defaultProp: defaultOpenProp,
+    onChange: onOpenChangeProp,
+  });
+
+  return (
+    <PopoverContext.Provider value={{ open, setOpen }}>
+      <RadixPopover.Root open={open} onOpenChange={setOpen} {...props}>
+        {children}
+      </RadixPopover.Root>
+    </PopoverContext.Provider>
+  );
+};
+
+type TriggerProps = React.ComponentProps<typeof RadixPopover.Trigger> & {
+  tgphRef?: React.RefObject<HTMLButtonElement>;
+};
+
+const Trigger = ({
+  asChild = true,
+  tgphRef,
+  children,
+  ...props
+}: TriggerProps) => {
+  const context = React.useContext(PopoverContext);
+
+  return (
+    <RadixPopover.Trigger
+      onClick={() => {
+        context.setOpen(!context.open);
+      }}
+      asChild={asChild}
+      {...props}
+      ref={tgphRef}
+    >
+      <RefToTgphRef>{children}</RefToTgphRef>
+    </RadixPopover.Trigger>
+  );
+};
+
+type ContentProps<T extends TgphElement> = React.ComponentProps<
+  typeof RadixPopover.Content
+> &
+  Omit<TgphComponentProps<typeof Stack<T>>, "align"> & {
+    contentStackRef?: React.RefObject<HTMLDivElement>;
+  };
+
+const Content = <T extends TgphElement>({
+  direction = "column",
+  gap = "1",
+  rounded = "4",
+  py = "1",
+  border = "px",
+  shadow = "2",
+  side = "bottom",
+  sideOffset = 4,
+  align = "center",
+  bg = "surface-1",
+  alignOffset,
+  tgphRef,
+  children,
+  ...props
+}: ContentProps<T>) => {
+  const deriveAnimationBasedOnSide = (side: ContentProps<T>["side"]) => {
+    const ANIMATION_OFFSET = 5;
+    if (side === "top") {
+      return {
+        y: -ANIMATION_OFFSET,
+      };
+    }
+
+    if (side === "bottom") {
+      return {
+        y: ANIMATION_OFFSET,
+      };
+    }
+
+    if (side === "left") {
+      return {
+        x: -ANIMATION_OFFSET,
+      };
+    }
+
+    if (side === "right") {
+      return {
+        x: ANIMATION_OFFSET,
+      };
+    }
+  };
+
+  return (
+    <RadixPopover.Portal>
+      <RadixPopover.Content
+        asChild
+        side={side}
+        sideOffset={sideOffset}
+        align={align}
+        alignOffset={alignOffset}
+        {...props}
+        ref={tgphRef}
+      >
+        <RefToTgphRef>
+          <Stack
+            as={motion.div}
+            // Add tgph class so that this always works in portals
+            className="tgph"
+            initial={{
+              opacity: 0.5,
+              scale: 0.6,
+              ...deriveAnimationBasedOnSide(side),
+            }}
+            animate={{
+              opacity: 1,
+              scale: 1,
+              x: 0,
+              y: 0,
+            }}
+            transition={{ duration: 0.2, type: "spring", bounce: 0 }}
+            bg={bg}
+            direction={direction}
+            gap={gap}
+            rounded={rounded}
+            border={border}
+            py={py}
+            shadow={shadow}
+            style={{
+              overflowY: "auto",
+              transformOrigin: "var(--radix-tooltip-content-transform-origin)",
+            }}
+            zIndex="popover"
+          >
+            {children}
+          </Stack>
+        </RefToTgphRef>
+      </RadixPopover.Content>
+    </RadixPopover.Portal>
+  );
+};
+
+const Popover = {} as {
+  Root: typeof Root;
+  Trigger: typeof Trigger;
+  Content: typeof Content;
+};
+
+Object.assign(Popover, {
+  Root,
+  Trigger,
+  Content,
+});
+
+export { Popover };
