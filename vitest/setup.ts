113936a641a6761f9f60bbe97675237186ca3fa6
Matthew Mikolay <mmikolay@knock.app>
2025-02-20 09:36:52 -0500

    chore(KNO-7887): clean up vitest setup file (#418)

--- vitest/setup.ts
+++ vitest/setup.ts
@@ -1,22 +1,12 @@
 import "vitest";
-import { afterEach, expect } from "vitest";
+import { afterEach } from "vitest";
 
-import "@testing-library/jest-dom";
-import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
+import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
-import * as matchers from "@testing-library/jest-dom/matchers";
-
-expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
 });
-
-declare module "vitest" {
-  export interface Assertion<T = any> extends TestingLibraryMatchers<T, void> {}
-  export interface AsymmetricMatchersContaining<T = any>
-    extends TestingLibraryMatchers<T, void> {}
-}
 
 /*
  fixes: Error: Not implemented: HTMLCanvasElement.prototype.getContext (without installing the canvas npm package)
