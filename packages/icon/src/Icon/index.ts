cbc9b6a143fbbfe151a783dc3e044961a4080a8e
Mike Carbone <mfcbone@gmail.com>
2025-02-27 12:01:53 -0500

    export all the way

--- packages/icon/src/Icon/index.ts
+++ packages/icon/src/Icon/index.ts
@@ -1,1 +1,1 @@
-export { Icon } from "./Icon";
+export { Icon, type LucideIcon } from "./Icon";
--- packages/icon/src/index.ts
+++ packages/icon/src/index.ts
@@ -1,4 +1,4 @@
 // Export as "Lucide" so we have the potential
 // to support other icon libraries in the future
 export * as Lucide from "lucide-react";
-export { Icon } from "./Icon";
+export { Icon, type LucideIcon } from "./Icon";
