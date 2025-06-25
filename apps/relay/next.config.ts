00a29d9cea11b77b57bd6dabac40c99e930e8204
Kyle McDonald <kyle@knock.app>
2025-06-25 12:00:38 -0500

    fix: file tracing root + rename to relay

--- apps/relay/next.config.ts
+++ apps/relay/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  outputFileTracingRoot: path.join(__dirname, "../../"),
 };
 
 export default nextConfig;
