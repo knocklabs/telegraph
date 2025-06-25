151e97056d16488b0f3d623a0c7ca7c40e12ec26
Kyle McDonald <kyle@knock.app>
2025-06-25 14:40:12 -0500

    fix: try webpack

--- apps/relay/next.config.ts
+++ apps/relay/next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   outputFileTracingIncludes: {
     "/api/**": ["../../packages", "../../packages/**/*"],
+  },
+  outputFileTracingExcludes: {
+    "/api/**": [
+      "../../packages/**/dist/**",
+      "../../packages/**/node_modules/**",
+    ],
   },
 };
 
