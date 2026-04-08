2edcf0e2cde87a7a3be95d7fefce1824ca5174eb
Krisna <krisna@knock.app>
2026-04-08 13:23:54 -0500

    fix: button default props

--- packages/button/src/Button/Button.tsx
+++ packages/button/src/Button/Button.tsx
@@ -54,8 +54,8 @@ export type RootProps<T extends TgphElement = "button"> = Omit<
 const ButtonContext = React.createContext<
   Required<Omit<RootBaseProps, "color" | "as" | "state"> & InternalProps>
 >({
-  variant: "solid",
-  size: "2",
+  variant: "outline",
+  size: "1",
   color: "default",
   state: "default",
   layout: "default",
@@ -78,8 +78,8 @@ const deriveState = (params: DeriveStateParams): InternalProps["state"] => {
 
 const Root = <T extends TgphElement = "button">({
   as,
-  variant = "solid",
-  size = "2",
+  variant = "outline",
+  size = "1",
   color = "default",
   state: stateProp = "default",
   active = false,
