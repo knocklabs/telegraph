40a19bf87349bdd14eef8f819ef66d6666bc88a7
BenaventeX24 <benaventematias04@gmail.com>
2026-07-09 14:42:02 -0300

    chore: use borderRightRadius instead

--- packages/combobox/src/Combobox/Combobox.primitives.tsx
+++ packages/combobox/src/Combobox/Combobox.primitives.tsx
@@ -350,8 +350,7 @@ const TriggerTagButton = <T extends TgphElement>({
     <Tag.Button
       icon={{ icon: X, alt: `Remove ${triggerTagContext.value}` }}
       height="full"
-      borderTopRightRadius="1"
-      borderBottomRightRadius="1"
+      borderRightRadius="1"
       onClick={(event: React.MouseEvent) => {
         if (!context.onValueChange) return;
         const onValueChange =
